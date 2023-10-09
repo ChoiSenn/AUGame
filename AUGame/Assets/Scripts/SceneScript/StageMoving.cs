8da37d7bbf1a0397bc0e16d6ceade2f657108514
ChoiSenn <quitechoisen@gmail.com>
2023-10-09 22:38:45 +0900

    xds

---
+++ AUGame/Assets/Scripts/SceneScript/StageMoving.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageMoving : MonoBehaviour
+{
+    Vector4 m_vecMouseDownPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            m_vecMouseDownPos = Input.mousePosition;
+
+            // 마우스 클릭 위치를 카메라 스크린 월드포인트로 변경
+            Vector2 pos = Camera.main.ScreenToWorldPoint(m_vecMouseDownPos);
+
+            // Raycast함수를 통해 부딪치는 collider를 hit에 리턴받기
+            RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                if (hit.collider.name == "Coding1StageOn")
+                {
+                    SceneManager.LoadScene("Coding1Stage");
+                }
+
+            }
+        }
+    }
+}
